206b1fe5118b6208f18be6200402eb07660497b8
Fonzy57 <traben@hotmail.fr>
2025-03-04 16:05:42 +0100

    Remove id from sql file

--- src/main/resources/data-donnees-test.sql
+++ src/main/resources/data-donnees-test.sql
@@ -1,5 +1,5 @@
-INSERT INTO produit (id, nom, code, description, prix)
-VALUES (1, 'Pepsi', 'pep', 'Bouteille de 1.5 L', 1.89),
-       (2, 'Coca', 'coc', 'Bouteille de 2.0 L', 2.50),
-       (3, 'Sprite', 'spr', 'Cannette de 33 cL', 1.25),
-       (4, '7up', 'zup', 'Bouteille 1.5 L', 1.75);
+INSERT INTO produit (nom, code, description, prix)
+VALUES ('Pepsi', 'pep', 'Bouteille de 1.5 L', 1.89),
+       ('Coca', 'coc', 'Bouteille de 2.0 L', 2.50),
+       ('Sprite', 'spr', 'Cannette de 33 cL', 1.25),
+       ('7up', 'zup', 'Bouteille 1.5 L', 1.75);
