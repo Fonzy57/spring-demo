d8e5940aae5901ff29659e24afd7263abaa80ff5
Fonzy57 <traben@hotmail.fr>
2025-03-04 10:51:48 +0100

    Use DAO to find all products

--- src/main/java/com/site/cda_demo/controller/ProduitController.java
+++ src/main/java/com/site/cda_demo/controller/ProduitController.java
@@ -1,14 +1,18 @@
 package com.site.cda_demo.controller;
 
+import com.site.cda_demo.dao.ProduitDao;
 import com.site.cda_demo.model.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ProduitController {
+
+  @Autowired // Donne à ProduitController le ProduitDao
+  protected ProduitDao produitDao;
 
   @GetMapping("/pepsi")
   public Produit get() {
@@ -25,20 +29,22 @@ public class ProduitController {
   @GetMapping("/produits")
   public List<Produit> getAll() {
 
-    List<Produit> produits = new ArrayList<Produit>();
+//    List<Produit> produits = new ArrayList<Produit>();
+//
+//    Produit pepsi = new Produit();
+//    pepsi.setNom("Pepsi");
+//    pepsi.setDescription("Meilleur soda au monde");
+//
+//    Produit coca = new Produit();
+//    coca.setNom("Coca-Cola");
+//    coca.setDescription("Comme Pepsi mais beaucoup moins bon...");
+//
+//    produits.add(pepsi);
+//    produits.add(coca);
+//
+//    return produits;
 
-    Produit pepsi = new Produit();
-    pepsi.setNom("Pepsi");
-    pepsi.setDescription("Meilleur soda au monde");
-
-    Produit coca = new Produit();
-    coca.setNom("Coca-Cola");
-    coca.setDescription("Comme Pepsi mais beaucoup moins bon...");
-
-    produits.add(pepsi);
-    produits.add(coca);
-
-    return produits;
+    return produitDao.findAll();
   }
 
 }
