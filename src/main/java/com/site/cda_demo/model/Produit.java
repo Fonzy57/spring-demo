04018d337b512a204ef60a9636e9b6f544eeac81
Fonzy57 <traben@hotmail.fr>
2025-03-04 16:05:58 +0100

    Add auto increment for product id

--- src/main/java/com/site/cda_demo/model/Produit.java
+++ src/main/java/com/site/cda_demo/model/Produit.java
@@ -1,8 +1,6 @@
 package com.site.cda_demo.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +10,7 @@ import lombok.Setter;
 public class Produit {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   protected Integer id;
 
   @Column(nullable = false)
