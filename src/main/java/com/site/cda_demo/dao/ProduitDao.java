daac181473a71dce50013b6d372f5ef617fb64fd
Fonzy57 <traben@hotmail.fr>
2025-03-04 10:51:19 +0100

    Add Produit DAO interface

---
+++ src/main/java/com/site/cda_demo/dao/ProduitDao.java
@@ -0,0 +1,11 @@
+package com.site.cda_demo.dao;
+
+import com.site.cda_demo.model.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Crée une dépendance
+public interface ProduitDao extends JpaRepository<Produit, Integer> {
+
+
+}
