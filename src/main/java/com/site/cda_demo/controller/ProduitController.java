861bced850985f69f72258fb0ca98ac496c43ea2
Fonzy57 <traben@hotmail.fr>
2025-03-04 13:39:03 +0100

    Change Produit Controller

--- src/main/java/com/site/cda_demo/controller/ProduitController.java
+++ src/main/java/com/site/cda_demo/controller/ProduitController.java
@@ -3,48 +3,39 @@ package com.site.cda_demo.controller;
 import com.site.cda_demo.dao.ProduitDao;
 import com.site.cda_demo.model.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProduitController {
-
-  @Autowired // Donne à ProduitController le ProduitDao
+  
   protected ProduitDao produitDao;
 
-  @GetMapping("/pepsi")
-  public Produit get() {
+  @Autowired // Donne à ProduitController le ProduitDao
+  public ProduitController(ProduitDao produitDao) {
+    this.produitDao = produitDao;
+  }
 
-    Produit pepsi = new Produit();
-    pepsi.setId(1);
-    pepsi.setNom("Pepsi");
-    pepsi.setPrix(100);
-    pepsi.setDescription("Meilleur soda au monde");
+  @GetMapping("/produit/{id}")
+  public ResponseEntity<Produit> get(@PathVariable int id) {
 
-    return pepsi;
+    Optional<Produit> optionalProduit = produitDao.findById(id);
+
+    if (optionalProduit.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    return new ResponseEntity<>(optionalProduit.get(), HttpStatus.OK);
   }
 
   @GetMapping("/produits")
   public List<Produit> getAll() {
-
-//    List<Produit> produits = new ArrayList<Produit>();
-//
-//    Produit pepsi = new Produit();
-//    pepsi.setNom("Pepsi");
-//    pepsi.setDescription("Meilleur soda au monde");
-//
-//    Produit coca = new Produit();
-//    coca.setNom("Coca-Cola");
-//    coca.setDescription("Comme Pepsi mais beaucoup moins bon...");
-//
-//    produits.add(pepsi);
-//    produits.add(coca);
-//
-//    return produits;
-
     return produitDao.findAll();
   }
-
 }
