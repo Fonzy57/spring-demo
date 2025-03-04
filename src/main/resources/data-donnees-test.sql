9df8a2246e9d8c89efee10dd242ad42407e4a1a2
Fonzy57 <traben@hotmail.fr>
2025-03-04 10:52:36 +0100

    Add sql file for seeding DB on program launch

---
+++ src/main/resources/data-donnees-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO produit (id, nom, code, description, prix)
+VALUES (1, 'Pepsi', 'pep', 'Bouteille de 1.5 L', 1.89),
+       (2, 'Coca', 'coc', 'Bouteille de 2.0 L', 2.50),
+       (3, 'Sprite', 'spr', 'Cannette de 33 cL', 1.25),
+       (4, '7up', 'zup', 'Bouteille 1.5 L', 1.75);
