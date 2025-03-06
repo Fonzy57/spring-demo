790d9a208712bd6364fe2a8473415a2cf6882c77
Fonzy57 <traben@hotmail.fr>
2025-03-06 09:53:34 +0100

    Change SQL order to avoid error

--- src/main/resources/data-donnees-test.sql
+++ src/main/resources/data-donnees-test.sql
@@ -1,10 +1,10 @@
-INSERT INTO produit (nom, code, description, prix)
-VALUES ('Pepsi', 'pep', 'Bouteille de 1.5 L', 1.89),
-       ('Coca', 'coc', 'Bouteille de 2.0 L', 2.50),
-       ('Sprite', 'spr', 'Cannette de 33 cL', 1.25),
-       ('7up', 'zup', 'Bouteille 1.5 L', 1.75);
-
 INSERT INTO etat (nom)
 VALUES ('neuf'),
        ('occasion'),
        ('bon');
+
+INSERT INTO produit (nom, code, description, prix, etat_id)
+VALUES ('Pepsi', 'pep', 'Bouteille de 1.5 L', 1.89, 1),
+       ('Coca', 'coc', 'Bouteille de 2.0 L', 2.50, 2),
+       ('Sprite', 'spr', 'Cannette de 33 cL', 1.25, 3),
+       ('7up', 'zup', 'Bouteille 1.5 L', 1.75, 1);
