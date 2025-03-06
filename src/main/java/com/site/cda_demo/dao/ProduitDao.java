95a663e0bae451a6342b95a06d24ee3602cf9b97
Fonzy57 <traben@hotmail.fr>
2025-03-06 09:54:18 +0100

    Add comment

--- src/main/java/com/site/cda_demo/dao/ProduitDao.java
+++ src/main/java/com/site/cda_demo/dao/ProduitDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository // Crée une dépendance
-public interface ProduitDao extends JpaRepository<Produit, Integer> {
+public interface ProduitDao extends JpaRepository<Produit, Integer> { // Integer c'est le type de l'ID
 }
