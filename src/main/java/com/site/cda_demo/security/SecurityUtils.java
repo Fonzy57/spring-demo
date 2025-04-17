826605931576e6b929160354b7fa5d06813cea20
Fonzy57 <traben@hotmail.fr>
2025-04-17 08:52:54 +0200

    Add method for getting role

--- src/main/java/com/site/cda_demo/security/SecurityUtils.java
+++ src/main/java/com/site/cda_demo/security/SecurityUtils.java
@@ -4,12 +4,23 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
-public class JwtUtils {
+public class SecurityUtils {
+
+  public String getRole(AppUserDetails userDetails) {
+    return userDetails.getAuthorities()
+        .stream()
+        .map(r -> r.getAuthority())
+        .findFirst()
+        .orElse(null);
+  }
 
   public String generateToken(AppUserDetails userDetails) {
     return Jwts.builder()
         .setSubject(userDetails.getUsername())
+        .addClaims(Map.of("role", getRole(userDetails)))
         .signWith(SignatureAlgorithm.HS256, "azerty")
         // .setExpiration() si on veut gérer l'expiration
         .compact();
